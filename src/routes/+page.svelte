284132f1a94842b3c79ee0d93ca643f199afb643
Kush Agarwal <kushhansagarwal@gmail.com>
2023-06-06 20:55:01 -0700

    updated for group

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,6 @@
 	import { json } from '@sveltejs/kit';
 	import { text } from './stores';
 
-	function parseDateTimeString(dateTimeString: string): Date {
-		// remove first and last character from the string
-		dateTimeString = dateTimeString.slice(1, -1);
-		const dateParts = dateTimeString.split(',');
-		const date = dateParts[0].split('/');
-		const time = dateParts[1].split(':');
-
-		// Validate the date parts
-		if (dateParts.length !== 2) {
-			throw new Error('Invalid date string');
-		}
-
-		const month = parseInt(date[0]);
-		const day = parseInt(date[1]);
-		const year = parseInt(20 + date[2]);
-		const hour = parseInt(time[0]);
-		const minute = parseInt(time[1]);
-		const second = parseInt(time[2]);
-		console.log(dateTimeString, year, month, day, hour, minute, second);
-
-		return new Date(year, month - 1, day, hour, minute, second);
-	}
-
 	async function getTexts() {
 		// const res = await fetch('http://127.0.0.1:5000/query', {
 		// 	method: 'POST',
@@ -43,7 +20,7 @@
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				collection_name: 'Text',
+				chat: "IM",
 				query: $text
 			})
 		});
@@ -63,6 +40,8 @@
 		time: string;
 		sender: string;
 		content: string;
+		image: string;
+		chat: string;
 		_additional: string;
 	}
 	var texts: Text[] = [];
@@ -116,9 +95,7 @@
 						<div class="chat-image avatar">
 							<div class="w-10 rounded-full">
 								<img
-									src={text.sender == 'Sananshi Pidyar'
-										? 'https://pps.whatsapp.net/v/t61.24694-24/146671092_640994244434955_6606309732909724688_n.jpg?ccb=11-4&oh=01_AdR6pMOe9PffO4sJXDR5pIc-LeWBLmGO2zJPcZkW3adDlA&oe=648B2AAC'
-										: 'https://pps.whatsapp.net/v/t61.24694-24/319948242_206350655299624_4193892322770336898_n.jpg?ccb=11-4&oh=01_AdRxMNr96OtwCLf6aFpW3mBtOVMeV3pT4V7NFS3OCyE6Kg&oe=648BA891'}
+									src={text.image}
 									alt="img"
 								/>
 							</div>
@@ -128,7 +105,7 @@
 						</div>
 						<div class="chat-bubble bg-base-100 text-left">{text.content}</div>
 						<div class="chat-footer opacity-50">
-							{new Date(parseDateTimeString(text.time)).toLocaleString()}
+							{text.time}
 						</div>
 					</div>
 				</div>
--- src/routes/api/query/+server.ts
+++ src/routes/api/query/+server.ts
@@ -1,14 +1,17 @@
 import { json } from '@sveltejs/kit';
 import weaviate from 'weaviate-ts-client';
 import { API_KEY } from '$env/static/private';
+import { WEAVIATE } from '$env/static/private';
+// import { API_KEY } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-	const { query, collection_name } = await request.json();
+	const { query, chat } = await request.json();
 	const client = weaviate.client({
 		scheme: 'http',
-		host: 'chat-test-yw81eqbz.weaviate.network',
-		headers: { 'X-OpenAI-Api-Key': API_KEY }
+		host: 'text-gpt-3zss9391.weaviate.network',
+		headers: { 'X-OpenAI-Api-Key': API_KEY },
+		apiKey: new weaviate.ApiKey(WEAVIATE)
 	});
 
 	const nearText = {
@@ -19,31 +22,11 @@ export async function POST({ request }) {
 	const res = await client.graphql
 		.get()
 		.withClassName('Text')
-		.withFields('time sender content _additional { distance certainty }')
+		.withFields('time sender chat content image _additional { distance certainty }')
 		.withWhere({
-			operator: 'And',
-			operands: [
-				{
-					operator: 'Or',
-					operands: [
-						{
-							path: ['sender'],
-							operator: 'Equal',
-							valueText: 'Kush'
-						},
-						{
-							path: ['sender'],
-							operator: 'Equal',
-							valueText: 'Sananshi Pidyar'
-						}
-					]
-				},
-				{
-					path: ['content'],
-					operator: 'NotEqual',
-					valueText: 'image omitted'
-				}
-			]
+			path: ['chat'],
+			operator: 'Equal',
+			valueText: chat
 		})
 		.withNearText(nearText)
 		.withLimit(20)
