bd729c9041886ec15161950510ba2043832d7a52
Kush Agarwal <kushhansagarwal@gmail.com>
2023-06-06 23:37:15 -0700

    phone mockup for neighbours

--- src/components/ChatBubble.svelte
+++ src/components/ChatBubble.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import type { Text } from "svelte/types/compiler/interfaces";
-    interface Text {
+	import type { Text } from 'svelte/types/compiler/interfaces';
+	interface Text {
 		time: string;
 		sender: string;
 		content: string;
@@ -11,27 +11,26 @@
 		};
 	}
 
-    export let data: Text;
+	export let data: Text;
 </script>
 
 <div class="chat chat-start">
-						<div class="chat-image avatar">
-							<div class="w-10 rounded-full">
-								<img src={data.image} alt="img" />
-							</div>
-						</div>
-						<div class="chat-header">
-							{data.sender}
-							<time class="ml-1 text-xs opacity-50">{data._additional.certainty.toFixed(2)}</time>
-						</div>
-						<div class="chat-bubble bg-base-100 text-left">{data.content}</div>
-						<div class="chat-footer opacity-50">
-							{new Date(parseInt(data.time)).toLocaleDateString('en-US', {
-								weekday: 'long',
-								year: 'numeric',
-								month: 'long',
-								day: 'numeric'
-							})}
-						</div>
-					</div>
-
+	<div class="chat-image avatar">
+		<div class="w-10 rounded-full">
+			<img src={data.image} alt="img" />
+		</div>
+	</div>
+	<div class="chat-header">
+		{data.sender}
+		<!-- <time class="ml-1 text-xs opacity-50">{data._additional.certainty.toFixed(2) ?? 0}</time> -->
+	</div>
+	<div class="chat-bubble bg-base-100 text-left">{data.content}</div>
+	<div class="chat-footer opacity-50">
+		{new Date(parseInt(data.time)).toLocaleDateString('en-US', {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		})}
+	</div>
+</div>
